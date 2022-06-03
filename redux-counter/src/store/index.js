c95500805e257dddea4fbe269eef3a4e8db112a1
Nabin Khatri <submergednabin@gmail.com>
2022-06-03 13:04:45 -0400

    redux more changes on multiple states

--- redux-counter/src/store/index.js
+++ redux-counter/src/store/index.js
@@ -1,23 +1,44 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {counter: 0, showCounter: true}
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
     };
   }
 
   if (action.type=== "increaseby5"){
     return {
         counter: state.counter + action.value,
+        showCounter: state.showCounter
     }
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter
     };
   }
+
+  if(action.type === "toggle"){
+      return {
+          showCounter: !state.showCounter,
+          counter: state.counter
+      }
+  }
+
+  if(action.type === 'reset'){
+      return {
+          showCounter: state.showCounter,
+          counter: 0
+      }
+  }
+
+//   if(action.type === "")
   return state;
 };
 
